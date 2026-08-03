53c42cd7bcc07dc2761e856a96224d64ac22e6c6
rashhplap <rashhplap@gmail.com>
2026-08-03 05:55:42 +0530

    update: official Round 3 cutoffs and SC-2 sub-category selection

--- inspect_cutoff.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const cutoffText = fs.readFileSync(path.join(__dirname, 'docs', 'TS MBBS-College Wise Cut off (OU) ROUND-3_extracted.txt'), 'utf8');
-
-console.log("=== CUTOFF TEXT LENGTH ===", cutoffText.length);
-console.log(cutoffText.substring(0, 4000));
